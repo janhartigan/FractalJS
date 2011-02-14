cbdc90152b5d65da1c57f0cd01c240e202357c67
Jan Hartigan <janhartigan13@gmail.com>
2011-02-13 22:06:35 -0800

    create jquery plugin

---
+++ js/jQuery/jquery.fractaljs.js
@@ -0,0 +1,10 @@
+/** jQuery FractalJS plugin
+ * 
+ * This is a jQuery plugin that takes a canvas and turns it into a fractal drawer
+ * 
+ * @author Jan Hartigan
+ * @version 0.1.0 (2010-12-25)
+ */
+(function($) {
+	
+})();
